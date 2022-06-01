2cb8eded93e753a3d57dd44bf74e7732097bf24c
alba.lima.garcia <albalimagarcia@iessoterohernandez.es>
2022-06-01 14:22:06 +0200

    modificación module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module mangashelf {
     requires java.sql;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.mail;
+    requires activation;
 
     opens edu.fpdual.proyecto.mangashelf.JavaFX to javafx.fxml;
 
