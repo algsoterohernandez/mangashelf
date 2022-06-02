cf4c2e4ac85f9301c7db039856bde09194136291
alba.lima.garcia <albalimagarcia@iessoterohernandez.es>
2022-06-03 01:37:39 +0200

    añadiendo vista INFO y nuevos iconos

---
+++ src/main/java/edu/fpdual/proyecto/mangashelf/controller/InfoController.java
@@ -0,0 +1,9 @@
+package edu.fpdual.proyecto.mangashelf.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
+
+public class InfoController {
+
+}
