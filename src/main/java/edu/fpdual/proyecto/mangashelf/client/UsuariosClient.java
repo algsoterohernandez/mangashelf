2f1ac4c4e01320af20f9071d952e52475dcd85c4
Nicolás Rodríguez González <nicoassesin@gmail.com>
2022-06-03 12:46:41 +0200

    Creacion de la base de las funciones de RegistroLogin (sin consultas)

--- src/main/java/edu/fpdual/proyecto/mangashelf/client/UsuariosClient.java
+++ src/main/java/edu/fpdual/proyecto/mangashelf/client/UsuariosClient.java
@@ -1,7 +1,6 @@
 package edu.fpdual.proyecto.mangashelf.client;
 
 import edu.fpdual.proyecto.mangashelf.controller.dto.Autor;
-import edu.fpdual.proyecto.mangashelf.controller.dto.Usuarios;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
--- src/main/java/edu/fpdual/proyecto/mangashelf/controller/RegistroLoginController.java
+++ src/main/java/edu/fpdual/proyecto/mangashelf/controller/RegistroLoginController.java
@@ -1,16 +1,13 @@
 package edu.fpdual.proyecto.mangashelf.controller;
 
-import edu.fpdual.proyecto.mangashelf.client.UsuariosClient;
-import edu.fpdual.proyecto.mangashelf.controller.dto.Autor;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
-import jakarta.ws.rs.client.WebTarget;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
-import java.util.LinkedHashSet;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RegistroLoginController {
 
@@ -27,11 +24,18 @@ public class RegistroLoginController {
     private TextField contrasenyaRegistro;
 
     @FXML
+    private Label comentarioRegistro;
+
+    @FXML
     private TextField emailInicio;
 
     @FXML
     private TextField contrasenyaInicio;
 
+    @FXML
+    private Label comentarioInicio;
+
+    //Borra los datos introducidos en la ventana de registro
     @FXML
     private void borrarRegistrar() throws IOException {
 
@@ -40,6 +44,7 @@ public class RegistroLoginController {
 
     }
 
+    //Borra los datos introducidos en la ventana de inicio de sesion
     @FXML
     private void borrarIniciar() throws IOException {
 
@@ -48,6 +53,7 @@ public class RegistroLoginController {
 
     }
 
+    //Cambia a la ventana de iniciar sesion y borra los datos introducidos en la de registro
     @FXML
     private void cambiarInicioSesion() throws IOException {
 
@@ -58,23 +64,68 @@ public class RegistroLoginController {
 
     }
 
+    //Comprueba si el usuario ya posee una cuenta, si la tiene mostrara que ya tiene una cuenta,
+    //si no, le registrara en la base de datos y abrira la main
     @FXML
     private void registrarUsuario() throws IOException {
 
         emailUsuario = emailRegistro.getText();
         contrasenyaUsuario = contrasenyaRegistro.getText();
 
-        LinkedHashSet<Autor> autores = new UsuariosClient().findAll();
-
-        System.out.println(autores);
     }
 
+    //Comprueba si el usuario ya posee una cuenta, si la tiene abrira la main,
+    //si no, mostrara un mensaje de que esa cuenta no existe
     @FXML
     private void iniciarSesionUsuario() throws IOException {
 
         emailUsuario = emailInicio.getText();
         contrasenyaUsuario = contrasenyaInicio.getText();
 
+    }
+
+    //Si el usuario olvida su contrasenya, se genera una aleatoria, se actualiza en su base de datos y
+    //se le envia un correo con la nueva
+    @FXML
+    private void generarContrasenya() throws IOException {
+
+        emailUsuario = emailInicio.getText();
+
+        if (emailUsuario.equals("")) {
+
+            comentarioInicio.setTextFill(Color.RED);
+            comentarioInicio.setText("Introduzca un email para enviarle una nueva contraseña");
+
+        } else {
+
+            String nuevaContrasenya = cadenaAleatoria(8);
+
+            comentarioInicio.setTextFill(Color.BLACK);
+            comentarioInicio.setText("Se ha enviado una nueva contraseña al email "+emailUsuario+" "+nuevaContrasenya);
+
+        }
+
+    }
+
+    //Generador de contrasenyas aleatorias
+    public static String cadenaAleatoria(int longitud) {
+
+        String caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+        String nuevaContrasenya = "";
+
+        for (int x = 0; x < longitud; x++) {
+            int indiceAleatorio = numeroAleatorioEnRango(0, caracteres.length() - 1);
+            char caracterAleatorio = caracteres.charAt(indiceAleatorio);
+            nuevaContrasenya += caracterAleatorio;
+        }
+
+        return nuevaContrasenya;
+    }
+
+    public static int numeroAleatorioEnRango(int minimo, int maximo) {
+
+        return ThreadLocalRandom.current().nextInt(minimo, maximo + 1);
     }
 
 }
