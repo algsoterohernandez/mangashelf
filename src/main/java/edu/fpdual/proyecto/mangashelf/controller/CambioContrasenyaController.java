a4b1fe959c1fb1694df2a963bb8572fde5d6ca24
Nicolás Rodríguez González <nicoassesin@gmail.com>
2022-06-04 14:19:08 +0200

    Creación de las bases de las funciones de CambioContrasenyaController

--- src/main/java/edu/fpdual/proyecto/mangashelf/controller/CambioContrasenyaController.java
+++ src/main/java/edu/fpdual/proyecto/mangashelf/controller/CambioContrasenyaController.java
@@ -1,8 +1,10 @@
 package edu.fpdual.proyecto.mangashelf.controller;
 
+import edu.fpdual.proyecto.mangashelf.Mangashelf;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 
@@ -15,16 +17,48 @@ public class CambioContrasenyaController {
     private PasswordField confirmacionContrasenya;
 
     @FXML
-    private Button enviarCambioBoton;
+    private Label mensajeCambio;
 
     @FXML
-    private Button borrarCambioBoton;
+    private void volverIndiceCambio() throws IOException{
+
+        Mangashelf.setRoot("Main");
+
+    }
 
     @FXML
     private void borrarCambioContrasenya() throws IOException{
 
         nuevaContrasenya.clear();
         confirmacionContrasenya.clear();
+
+    }
+
+    @FXML
+    private void enviarCambioContrasenya() throws IOException{
+
+        if (nuevaContrasenya.getText().equals(confirmacionContrasenya.getText())) {
+
+            //Aqui se debe realizar la consulta del cambio de contraseña
+
+            mensajeCambio.setTextFill(Color.BLACK);
+            mensajeCambio.setText("Contraseña actualizada correctamente");
+
+        } else {
+
+            mensajeCambio.setTextFill(Color.RED);
+            mensajeCambio.setText("Las contraseñas no coinciden");
+
+        }
+
+    }
+
+    @FXML
+    private void eliminarCuenta() throws IOException{
+
+        //Aqui se debe realizar la consulta para eliminar cuenta
+
+        Mangashelf.setRoot("RegistroLogin");
 
     }
 
